2492ac790c7924333d90a2d1655287b8e0583110
Mario Zito <mario.zito@treme.io>
2024-04-05 20:08:44 -0300

    Add Activity page

---
+++ src/routes/(sidebar)/activity/Activity.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+  import ActivityPanel from './ActivityPanel.svelte';
+  import { activities } from './data';
+</script>
+
+<div class="p-4">
+  <ActivityPanel {activities}/>
+</div>
