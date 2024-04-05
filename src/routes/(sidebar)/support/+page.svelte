0a6cd0266a36a864bf7d7c3c53d622d41f83679b
Mario Zito <mario.zito@treme.io>
2024-04-05 20:09:11 -0300

    Add Support page

---
+++ src/routes/(sidebar)/support/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Support from "./Support.svelte";
+	import MetaTag from '../../utils/MetaTag.svelte';
+	//export let data;
+
+	const path: string = '/home';
+  const description: string = 'Admin Dashboard example using Flowbite Svelte';
+  const title: string = 'Flowbite Svelte Admin Dashboard - Dashboard';
+  const subtitle: string = 'Admin Dashboard';
+</script>
+
+<MetaTag {path} {description} {title} {subtitle} />
+
+<main class="p-4">
+  <Support />
+</main>
---
+++ src/routes/(sidebar)/support/Support.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+  import { Heading, P } from "flowbite-svelte";
+</script>
+
+<div class="p-4">
+  <Heading tag="h1">
+    Support
+  </Heading>  
+  <P class="text-gray-400 mb-4" size="base">
+    Ask for help and support 
+  </P>
+</div>
