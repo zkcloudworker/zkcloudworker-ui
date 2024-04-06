dfe084cc94f1d4e166536a09ea2467cd521374fa
Mario Zito <mario.zito@treme.io>
2024-04-06 20:05:58 -0300

    Deploy to vercel

--- svelte.config-spa.js
+++ svelte.config-spa.js
@@ -1,6 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +14,15 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		// adapter: adapter()
 		adapter: adapter({
-		})
+        // SPA options are shown. On some platforms
+        // these options are set automatically — see below
+        pages: 'build',
+        assets: 'build',
+        // fallback: undefined, // for static site
+        fallback: 'index.html', // for SPA
+        precompress: false,
+        strict: true
+		})    
 	}
 };
 
