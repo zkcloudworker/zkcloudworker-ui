f80d56ecdbe22a75d20d79b6d25a668aa67a3ebb
Mario Zito <mario.zito@treme.io>
2024-04-07 01:17:51 -0300

    Try deploy spa to vercel

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,8 @@
 // https://kit.svelte.dev/docs/single-page-apps     
+// for generating a static site
 export const ssr = false; 
+export const prerender = false;
+export const csr = true;
+
+// may need it for static sites ??? NOT SURE
+export const trailingSlash = 'always';
--- svelte.config-vercel.js
+++ svelte.config-vercel.js
@@ -1,12 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-  
 	preprocess: [vitePreprocess({})],
 
 	kit: {
@@ -15,15 +14,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		// adapter: adapter()
 		adapter: adapter({
-        // SPA options are shown. On some platforms
-        // these options are set automatically — see below
-        pages: 'build',
-        assets: 'build',
-        // fallback: undefined, // for static site
-        fallback: 'index.html', // for SPA
-        precompress: false,
-        strict: true
-		})    
+		})
 	}
 };
 
