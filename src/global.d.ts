f9d300173d3f3e7e2daa4b65a033f71d9f8fd4b0
leomanza <leomanzanal@gmail.com>
2024-04-09 10:31:14 -0300

    extract google analytics to layout + run on prod only

---
+++ src/global.d.ts
@@ -0,0 +1,4 @@
+interface ImportMetaEnv {
+    VITE_APP_ENV:string
+    VITE_GA_TAG:string
+  }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,18 @@
 	import { onMount } from 'svelte';
 
 	onMount(modeobserver);
+	
+	if (import.meta.env.VITE_APP_ENV === 'production') {
+		window.dataLayer = window.dataLayer || [];
+		window.gtag = function gtag(){window.dataLayer.push(arguments);}
+		window.gtag('js', new Date())
+
+		window.gtag('config', import.meta.env.VITE_GA_TAG);
+	}
 </script>
 
 <slot />
+
+<svelte:head>
+	<script async src={"https://www.googletagmanager.com/gtag/js?id="+import.meta.env.VITE_GA_TAG}></script>
+</svelte:head>
