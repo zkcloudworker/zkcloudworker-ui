7886a64923ccf2663a48e31ca3ec5b7403ddef0b
Mario Zito <mario.zito@treme.io>
2024-04-10 00:31:51 -0300

    Add refetchOnMount a query hooks

--- src/lib/hooks/communities.ts
+++ src/lib/hooks/communities.ts
@@ -4,14 +4,16 @@ import { getMyCommunities, getAllCommunities } from '$lib/api/queries';
 
 export function useGetMyCommunities() {
     return createQuery({
-        queryKey: ['my_communities'],
-        queryFn: () => getMyCommunities({}),
-      })
+      queryKey: ['my_communities'],
+      queryFn: () => getMyCommunities({}),
+      refetchOnMount: 'always'
+    })
 }
 
 export function useGetAllCommunities() {
     return createQuery({
-        queryKey: ['all_communities'],
-        queryFn: () => getAllCommunities({ notJoined: true }),
-      })
+      queryKey: ['all_communities'],
+      queryFn: () => getAllCommunities({ notJoined: true }),
+      refetchOnMount: 'always'
+    })
 }
