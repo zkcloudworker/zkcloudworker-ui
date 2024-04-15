70fb0f70befdf60337a3168753080e500b44893e
leomanza <leomanzanal@gmail.com>
2024-04-15 17:41:23 -0300

    final alignment

--- src/routes/(sidebar)/my-claims/ClaimsTableMobile.svelte
+++ src/routes/(sidebar)/my-claims/ClaimsTableMobile.svelte
@@ -20,36 +20,39 @@
 		{#each data as t}
 			<TableBodyRow>
 				<TableBodyCell>
-					<div class="flex space-x-4">
-						<Avatar src={t.image} class="round h-10 w-10 drop-shadow-lg" />
-						<div class="space-y-4">
-							<div class="flex items-center space-x-2">
-								<div class="grid grid-cols-1 gap-1">
-									<p class="text-weight-400 truncate text-xs text-gray-500">{t.community}</p>
-									<p class="text-weight-600 text-md truncate">{t.type}</p>
-								</div>
-							</div>
-							<div class="flex items-center justify-between">
-								<div class="flex space-x-4">
+					<div class="flex justify-between">
+						<div class="flex space-x-4">
+							<Avatar src={t.image} class="round h-10 w-10 drop-shadow-lg" />
+							<div class="space-y-4">
+								<div class="flex items-center space-x-2">
 									<div class="grid grid-cols-1 gap-1">
-										<p class="text-md text-weight-600 text-gray-500">Created</p>
-										<p class="text-xs"><Time timestamp={t.createdUTC} /></p>
-									</div>
-									<div class="grid grid-cols-1 gap-1 text-center">
-										<p class="text-md text-weight-600 text-gray-500">Claimed</p>
-										<p class="text-xs">
-											{#if t.issuedUTC}<Time timestamp={t.issuedUTC} />
-											{:else}-{/if}
-										</p>
+										<p class="text-weight-400 truncate text-xs text-gray-500">{t.community}</p>
+										<p class="text-weight-600 text-md truncate">{t.type}</p>
 									</div>
 								</div>
-								<StateBadge class="p-x-16 p-y-1 text-sm" data={t.state} />
+								<div class="flex items-center justify-between">
+									<div class="flex space-x-4">
+										<div class="grid grid-cols-1 gap-1">
+											<p class="text-md text-weight-600 text-gray-500">Created</p>
+											<p class="text-xs"><Time timestamp={t.createdUTC} /></p>
+										</div>
+										<div class="grid grid-cols-1 gap-1 text-center">
+											<p class="text-md text-weight-600 text-gray-500">Claimed</p>
+											<p class="text-xs">
+												{#if t.issuedUTC}<Time timestamp={t.issuedUTC} />
+												{:else}-{/if}
+											</p>
+										</div>
+										<div class="grid grid-cols-1 gap-1 text-center">
+											<p class="text-md text-weight-600 text-gray-500">State</p>
+											<StateBadge class="p-0 text-sm" data={t.state} />
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
+						<div><Button color="alternative" size="sm">Edit</Button></div>
 					</div>
-				</TableBodyCell>
-				<TableBodyCell>
-					<Button color="alternative" size="sm">Edit</Button>
 				</TableBodyCell>
 			</TableBodyRow>
 		{/each}
