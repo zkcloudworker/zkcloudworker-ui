e1f3aa3d096e28d5565c047d1723826015df7770
Mario Zito <mario.zito@treme.io>
2024-04-17 13:16:16 -0300

    Use .env.local for selecting api endpoint

--- src/lib/store/sessions.ts
+++ src/lib/store/sessions.ts
@@ -2,8 +2,8 @@
  * Manages the currently active session, stored in localStorage.
  * When signing out we need to clear it using removeActiveSession(). 
  */
-import { PUBLIC_API_HOST, PUBLIC_API_PORT, PUBLIC_API_PROTOCOL } from '$env/static/public';
 import type { Session } from "../types";
+import { getDefaultAPISessionFromEnv } from "./api";
 import { removeActiveUser } from "./profiles";
 
 export { 
@@ -15,22 +15,13 @@ export {
 
 const STORE_KEY = "current-session";
 
-const DEFAULT_SESSION: Session = {
-  host: PUBLIC_API_HOST, // "localhost"
-  protocol: PUBLIC_API_PROTOCOL as 'http' | 'https', // localhost uses "http"
-  port: Number(PUBLIC_API_PORT), // locahost uses 3080
-  authorization: "",
-  key: ""
-};
-
 function getCurrentSession(): Session | null {
   const data = localStorage.getItem(STORE_KEY);
   return data && JSON.parse(data) || null; 
 };
 
 function getDefaultSession(): Session {
-  console.log("store/sessions/getDefaultSession", DEFAULT_SESSION);
-  return DEFAULT_SESSION; 
+  return getDefaultAPISessionFromEnv(); 
 };
 
 function saveActiveSession(session: Session) {
