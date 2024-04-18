9a0e270a4fdc8b083a5977f2c392b3678af95983
Mario Zito <mario.zito@treme.io>
2024-04-18 20:48:48 -0300

    Fix default banner for CommunityCard

--- src/routes/(sidebar)/my-communities/CommunityCard.svelte
+++ src/routes/(sidebar)/my-communities/CommunityCard.svelte
@@ -1,42 +1,66 @@
 <script lang="ts">
+  import StateBadge from '$lib/components/StateBadge.svelte';
+	import { findState } from '$lib/types/states';
   import { Card, Badge, Avatar, Button, Img } from 'flowbite-svelte';
 
   export let 
     uid = '', title = '', description = '', image = '',
-    state = '', count = "0", 
+    state = '', count = "0", nClaims = 0, nCredentials = 0,
     joined = false;
 
-  const bgImage = '/images/gradient-background-1.jpeg';
+  $: sts = findState((state === 'INITIAL') ? 'Revision' : state);
+  const bgImage = '/images/socialcap-bg-signin.svg'; // gradient-background-1.jpeg';
+
+  function gotoLink(joined: boolean, uid: string) {
+    return joined 
+      ? `/community/${uid}`
+      : `/community/${uid}`;
+  }
 </script>
 
 <Card class="" padding="none" size="fluid">
   <!-- <div class="bg-[url(')] bg-cover p-4">
   </div> -->
-  <div class="relative">
+  <div class="relative bg-blue-100">
     <img src={bgImage} class="h-32 w-full"/>
-    <span class="inline-block mt-4 ms-4 absolute top-0 left-0">
+    <!-- <span class="inline-block mt-4 ms-4 absolute top-0 left-0">
       <Avatar src={image || bgImage} class="h-24 w-24 round drop-shadow-lg"/>
-    </span>
+    </span> -->
   </div>
 
-  <div class="flex flex-col px-6 pb-4">
-    <a href={`/community/${uid}`}>
-      <h6 class="mt-4 mb-2 text-xl font-bold text-gray-900 dark:text-white">{title}</h6>
+  <div class="px-4 pt-2 pb-4">
+    <!-- <Badge rounded border large color="green" class="inline-block mb-0">{state}</Badge> -->
+    <StateBadge data={sts} />
+    <a href={gotoLink(joined, uid)}>
+      <h6 class="mt-2 mb-2 text-xl font-bold text-gray-900 dark:text-white">{title}</h6>
     </a>
     <p class="mb-2 text-sm text-gray-600 dark:text-gray-400">
-      {description}
+      {description}                                              
     </p>
-    <div class="mt-4 justify-between items-center flex rtl:space-x-reverse">
-      <Badge rounded border large color="green">{state}</Badge>
-      <div class="flex">
-        <Avatar src="/images/profile-909.png" stacked size="sm"/>
-        <Avatar src="/images/profile-1127.png" stacked  size="sm"/>
-        <Avatar src="/images/profile-1.png" stacked  size="sm"/>
-        <Avatar stacked size="sm" class="bg-gray-700 text-white hover:bg-gray-600 text-xs">+{count}</Avatar>        
+    <div class="ms-0 mt-4 justify-between items-center flex rtl:space-x-reverse">
+      {#if joined}
+        <span class="inline-block text-sm font-bold">
+          <Badge class="inline-block mb-0 bg-gray-200 rounded-full">
+            { nClaims }
+          </Badge> claims
+        </span>
+        <span class="inline-block text-sm font-bold">
+          <Badge class="inline-block mb-0 bg-gray-200 rounded-full">
+            { nCredentials }
+          </Badge> issued
+        </span>
+      {/if}
+      <div class="flex ms-4">
+        <Avatar src="/images/gradient-lean.svg" stacked size="sm"/>
+        <Avatar src="/images/gradient-mario.svg" stacked  size="sm"/>
+        <Avatar src="/images/gradient-nicolas.svg" stacked  size="sm"/>
+        <Avatar stacked size="sm" class="bg-gray-700 text-white font-bold hover:bg-gray-600 text-xs">
+          {count}
+        </Avatar>        
       </div>      
       {#if !joined}
         <Button color="primary" size="sm">Join</Button>
       {/if}
-    </div>
+    </div>                
   </div>
 </Card>  
