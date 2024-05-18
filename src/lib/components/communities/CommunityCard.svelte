22e384942789ca3475e2b1b4fa4028b65474f491
Mario Zito <mario.zito@treme.io>
2024-05-17 21:28:51 -0300

    Add menu to Community Card

--- src/lib/components/communities/CommunityCard.svelte
+++ src/lib/components/communities/CommunityCard.svelte
@@ -2,11 +2,12 @@
   import StateBadge from '$lib/components/common/StateBadge.svelte';
 	import { findState } from '$lib/types/states';
   import { Card, Badge, Avatar, Button, Img } from 'flowbite-svelte';
+  import CommunityMenu from './CommunityMenu.svelte';
 
   export let 
     uid = '', title = '', description = '', image = '',
     state = '', count = "0", nClaims = 0, nCredentials = 0,
-    joined = false;
+    joined = false, isAdmin: boolean = false;
 
   $: sts = findState((state === 'INITIAL') ? 'Revision' : state);
   const bgImage = '/images/socialcap-bg-signin.svg'; // gradient-background-1.jpeg';
@@ -30,11 +31,17 @@
 
   <div class="px-4 pt-2 pb-4">
     <!-- <Badge rounded border large color="green" class="inline-block mb-0">{state}</Badge> -->
-    <StateBadge data={sts} />
+    <div class="flex items-center justify-between">
+      <StateBadge data={sts} />
+      <div>
+        <CommunityMenu {isAdmin} {joined} />
+      </div>
+    </div>
     <h6 class="mt-2 mb-2 text-xl font-bold text-blue-900 dark:text-white">{title}</h6>
     <p class="h-10 max-h-10 mb-6 text-sm text-gray-600 dark:text-gray-400 text-ellipsis overflow-hidden ">
       {description}
     </p>
+
     <div class="ms-0 mt-4 justify-between items-center flex rtl:space-x-reverse">
       {#if joined}
         <span class="inline-block text-sm font-bold">
