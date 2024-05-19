22d3d321fdfd39494cb913a1244fe712020f9627
Mario Zito <mario.zito@treme.io>
2024-05-19 01:24:54 -0300

    Show claimables in Community page

--- src/lib/components/common/NoData.svelte
+++ src/lib/components/common/NoData.svelte
@@ -3,24 +3,25 @@
 	import Icon from './Icon.svelte';
 	export let text = 'You have no data to display';
 	export let title = 'Nothing to see here';
+  export let buttons = true;
 </script>
 
-<div class="flex justify-center mt-8 lg:mt-20">
-	<Card class="items-center px-18 py-6 rounded-md border-0 bg-gray-50 text-center" padding="xl">
-
+<div class="flex justify-center mt-8 lg:mt-8">
+	<Card size="none" class="items-center px-18 py-6 rounded-md border-0 bg-gray-50 text-center" padding="xl">
 			<div class="mb-6">
-				<Icon name="Sad" size="8" />
+				<Icon name="Sad" size="16" />
 			</div>
 			<h6 class="text-md text-weight-500 mb-2 text-gray-900 dark:text-white">{title}</h6>
 			<p class="text-weight-400 mb-4 text-sm text-gray-600 dark:text-gray-400 sm:text-lg">
-				{text}
+				{@html text.replaceAll('\\n', '<br/>')}
 			</p>
-			<div
-				class="items-center justify-center space-y-4 sm:flex sm:space-x-4 sm:space-y-0 rtl:space-x-reverse"
-			>
-				<Button href="/" color="primary" size="sm">Back home</Button>
-				<Button color="none" class="border-0">Learn more</Button>
-			</div>
-
+      {#if buttons}
+        <div
+          class="items-center justify-center space-y-4 sm:flex sm:space-x-4 sm:space-y-0 rtl:space-x-reverse"
+        >
+          <Button href="/" color="primary" size="sm">Back home</Button>
+          <Button color="none" class="border-0">Learn more</Button>
+        </div>
+      {/if}  
 	</Card>
 </div>
