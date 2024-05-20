8060e08a4b194409ea9717e65e0bf4500d57d2b8
leomanza <leomanzanal@gmail.com>
2024-05-20 10:18:30 -0300

    claims row navigation

--- src/routes/(detail)/claim/[uid]/+page.js
+++ src/routes/(detail)/claim/[uid]/+page.js
@@ -1,8 +1,10 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, route, url }) {
   let planUid = url.searchParams.get('mp');
+  let mode = url.searchParams.get('mode');
   return {
     uid: params.uid,
-    planUid: planUid
+    planUid: planUid,
+    mode: mode
   };
 }
