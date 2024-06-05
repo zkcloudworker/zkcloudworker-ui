7370756dac755b8f2d0469be8927004b8f3160f2
leomanza <leomanzanal@gmail.com>
2024-06-05 18:45:38 -0300

    add gradient avatar

--- src/lib/components/common/gradient-svg.ts
+++ src/lib/components/common/gradient-svg.ts
@@ -4,6 +4,7 @@
  * - Some ideas: https://github.com/brandonsueur/react-gradient-avatar
 */
 import ColorHash from "color-hash";
+import { UID } from "$lib/types/uid";
 
 export function getInitials(name: string): string {
   const names = name.split(" ");
@@ -38,14 +39,14 @@ export function buildGradient(initials: string, size = 'lg') {
   
   function generateSVG(s: string, size = 256): string {
     const [c1, c2] = generateColours(s);
-  
+    const gradientId = `gradient-${UID.uuid4()}`
     const svg = `
       <svg width="${size}" height="${size}" viewBox="0 0 ${size} ${size}" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="${size / 2}" cy="${size / 2}" r="${
           size / 2
-        }" fill="url(#gradient)" />
+        }" fill="url(#${gradientId})" />
         <defs>
-          <linearGradient id="gradient" x1="0" y1="0" x2="${size}" y2="${size}" gradientUnits="userSpaceOnUse">
+        <linearGradient id="${gradientId}" x1="0" y1="0" x2="${size}" y2="${size}" gradientUnits="userSpaceOnUse">
             <stop stop-color="${c1}" />
             <stop offset="1" stop-color="${c2}" />
           </linearGradient>
