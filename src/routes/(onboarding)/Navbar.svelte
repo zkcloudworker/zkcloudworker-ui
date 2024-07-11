80d67e7a1339e7b58e3d4dc8f8d819be39ce3936
Mario Zito <mario.zito@treme.io>
2024-07-11 19:50:41 -0300

    Add Docs and Explore links to signup/login pages

--- src/routes/(onboarding)/Navbar.svelte
+++ src/routes/(onboarding)/Navbar.svelte
@@ -2,6 +2,7 @@
 	import {
 		NavBrand,
 		Navbar,
+    A
 	} from 'flowbite-svelte';
 	import '../../app.pcss';
 
@@ -10,7 +11,7 @@
 </script>
 
 <Navbar {fluid} class="text-black" color="default" let:NavContainer>
-	<NavContainer class="mb-px mt-px px-1 py-3 justify-center" {fluid}>
+	<NavContainer class="mb-px mt-px px-1 py-3 justify-between" {fluid}>
 		<NavBrand href="/" class={list ? 'w-60' : 'lg:w-60'}>
 			<img
         src="/images/zkCloudWorker-logo-horizontal.svg"
@@ -18,5 +19,9 @@
 				alt="Socialcap Logo"
 			/>
 		</NavBrand>
+    <div class="me-4 text-lg font-semibold">
+      <A class ="ms-6" href="https://docs.zkcloudworker.com/">Docs</A>
+      <A class ="ms-6" href="https://minarollupscan.com/">Explorer</A>
+    </div>
 	</NavContainer>
 </Navbar>
