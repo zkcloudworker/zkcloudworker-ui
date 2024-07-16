2bd0afa7deb5ce77becde9c7a085c910a9854d45
Mario Zito <mario.zito@treme.io>
2024-07-15 21:00:21 -0300

    Add links to support page (needs more love !)

--- src/routes/(hub)/support/Support.svelte
+++ src/routes/(hub)/support/Support.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-  import { Heading, P } from "flowbite-svelte";
+  import { Heading, P, A } from "flowbite-svelte";
   import H1 from "$lib/components/common/H1.svelte";
 </script>
 
 <div>
-  <P class="text-gray-400 mt-8" size="base">
-    Faq goes here
-    </P>
-    <P class="text-gray-400 mt-8" size="base">
-      Contact information goes here ...
-    </P>
+  <P class="text-gray-400 mt-8 leading-loose text-lg leading-[48px]">
+    Documentation: <A href="https://docs.zkcloudworker.com">docs.zkcloudworker.com</A>
+    <br>
+    Support: <A href="mailto: support@zkcloudworker.com">support@zkcloudworker.com</A>
+    <br>
+    <A href="https://discord.gg/j8XpQ3pr">Discord</A>
+    <br/>
+    <A href=" https://github.com/dfstio">Github</A>
+    <br/>
+    <A href="https://twitter.com/dfst_io">Twitter</A>
+    <br/>
+    <br>
+  </P>
 </div>
